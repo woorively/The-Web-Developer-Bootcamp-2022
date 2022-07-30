f3f570d0f3a1c472d6f25ddd945ce7279f5b141d
woorively <woorively@gmail.com>
2022-07-30 23:22:11 +0900

    Loops

---
+++ JS_Loops/ForLoops/app.js
@@ -0,0 +1,20 @@
+for (let num = 1; num <= 10; num += 1) {
+    console.log(num);
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i);
+}
+
+for (let i = 10; i <= 1000; i *= 10) {
+    console.log(i);
+}
+
+// 무한 루프
+// for (let i = 20; i >= 0; i++) {
+//     console.log(i)
+// }
